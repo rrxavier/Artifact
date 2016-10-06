10dad951b175cd4f69e8a314b98904eb6206248d
svenwikberg <sven.wkbrg@eduge.ch>
2016-10-06 14:11:18 +0200

    RemplissageAppearrance1

--- Doc/scriptRemplissage/remplissageAppearance.sql
+++ Doc/scriptRemplissage/remplissageAppearance.sql
@@ -1,54 +1,114 @@
-INSERT INTO `appearance` VALUES (NULL, "The Silver Hand", "theSilverHand_yellow.jpg", 1), 
-                (NULL, "Fist of the Fallen Watcher", "fistOfTheFallenWatcher_yellow.jpg", 1), 
-                (NULL, "Protector's Judgment", "protectorSJudgment_blue.jpg", 1), 
-                (NULL, "Gravewarder", "gravewarder_blue.jpg", 1);
+INSERT INTO `appearance` VALUES (NULL, "The Silver Hand", "theSilverHand_1.jpg", 1), 
+                                (NULL, "Fist of the Fallen Watcher", "fistOfTheFallenWatcher_1.jpg", 1), 
+                                (NULL, "Protector's Judgment", "protectorSJudgment_1.jpg", 1), 
+                                (NULL, "Gravewarder", "gravewarder_1.jpg", 1);
 
 INSERT INTO `appearance` VALUES (NULL, "Truthguard", "truthguard_white.jpg", 2), 
-                (NULL, "Light of the Titans", "lightOfTheTitans_blue.jpg", 2), 
-                (NULL, "Divine Protector", "divineProtector_red.jpg", 2), 
-                (NULL, "Dark Keeper's Ward", "darkKeeperSWard_green.jpg", 2);
+                                (NULL, "Light of the Titans", "lightOfTheTitans_1.jpg", 2), 
+                                (NULL, "Divine Protector", "divineProtector_1.jpg", 2), 
+                                (NULL, "Dark Keeper's Ward", "darkKeeperSWard_1.jpg", 2);
 
-INSERT INTO `appearance` VALUES (NULL, "Ashbringer", "ashbringer_red.jpg", 3), 
-                (NULL, "Greatsword of the Righteous", "greatswordofTheRighteous_yellow.jpg", 3), 
-                (NULL, "Burning Reprisal", "burningReprisal_orange.jpg", 3), 
-                (NULL, "Fallen Hope", "fallenHope_purple.jpg", 3);
+INSERT INTO `appearance` VALUES (NULL, "Ashbringer", "ashbringer_1.jpg", 3), 
+                                (NULL, "Greatsword of the Righteous", "greatswordofTheRighteous_1.jpg", 3), 
+                                (NULL, "Burning Reprisal", "burningReprisal_1.jpg", 3), 
+                                (NULL, "Fallen Hope", "fallenHope_1.jpg", 3);
 
-INSERT INTO `appearance` VALUES (NULL, "Stromkar, the Warbreaker", "stromkar_yellow.jpg", 4), 
-                (NULL, "Vengeance of the Fallen", "vengeanceOfTheFallen_yellow.jpg", 4), 
-                (NULL, "Flamereaper", "flamereaper_yellow.jpg", 4), 
-                (NULL, "Wrath's Edge", "wrathSEdge_green.jpg", 4);
+INSERT INTO `appearance` VALUES (NULL, "Stromkar, the Warbreaker", "stromkar_1.jpg", 4), 
+                                (NULL, "Vengeance of the Fallen", "vengeanceOfTheFallen_1.jpg", 4), 
+                                (NULL, "Flamereaper", "flamereaper_1.jpg", 4), 
+                                (NULL, "Wrath's Edge", "wrathSEdge_1.jpg", 4);
 
-INSERT INTO `appearance` VALUES (NULL, "Warswords of the Valarjar", "warswordOdTheValarjar_yellow.jpg", 5), 
-                (NULL, "Arm of the Dragonrider", "armOfTheDragonRider_yellow.jpg", 5), 
-                (NULL, "Valormaw", "valormaw_red.jpg", 5), 
-                (NULL, "Stormbreath", "stormbreath_blue.jpg", 5);
+INSERT INTO `appearance` VALUES (NULL, "Warswords of the Valarjar", "warswordOdTheValarjar_1.jpg", 5), 
+                                (NULL, "Arm of the Dragonrider", "armOfTheDragonRider_1.jpg", 5), 
+                                (NULL, "Valormaw", "valormaw_1.jpg", 5), 
+                                (NULL, "Stormbreath", "stormbreath_1.jpg", 5);
 
-INSERT INTO `appearance` VALUES (NULL, "Scale of the Earth-Warder", "scaleOfTheEarthWarder_purple.jpg", 6), 
-                (NULL, "Arm of the Fallen King", "armOfTheFallenKing_purple.jpg", 6), 
-                (NULL, "Unbroken Stand", "unbrokenStand_blue.jpg", 6), 
-                (NULL, "Deathguard's Gaze", "deathguardSGaze_green.jpg", 6);
+INSERT INTO `appearance` VALUES (NULL, "Scale of the Earth-Warder", "scaleOfTheEarthWarder_1.jpg", 6), 
+                                (NULL, "Arm of the Fallen King", "armOfTheFallenKing_1.jpg", 6), 
+                                (NULL, "Unbroken Stand", "unbrokenStand_1.jpg", 6), 
+                                (NULL, "Deathguard's Gaze", "deathguardSGaze_1.jpg", 6);
 
-INSERT INTO `appearance` VALUES (NULL, "Titanstrike", "titanstrike_blue.jpg", 7), 
-                (NULL, "Eaglewatch", "eaglewatch_blue.jpg", 7), 
-                (NULL, "Elekk's Thunder", "elekkSThunder_purple.jpg", 7), 
-                (NULL, "Boarshot Cannon", "boarshotCannon_yellow.jpg", 7);
+INSERT INTO `appearance` VALUES (NULL, "Titanstrike", "titanstrike_1.jpg", 7), 
+                                (NULL, "Eaglewatch", "eaglewatch_1.jpg", 7), 
+                                (NULL, "Elekk's Thunder", "elekkSThunder_1.jpg", 7), 
+                                (NULL, "Boarshot Cannon", "boarshotCannon_1.jpg", 7);
 
-INSERT INTO `appearance` VALUES (NULL, "Thas'dorah, Legacy of the Windrunners", "thasDorah_green.jpg", 8), 
-                (NULL, "A Sister's Bond", "aSisterSBond_purple.jpg", 8), 
-                (NULL, "Phoenix's Rebirth", "phoenixSRebirth_yellow.jpg", 8), 
-                (NULL, "Ranger-General's Guard", "rangerGeneralSGuard_yellow.jpg", 8);
+INSERT INTO `appearance` VALUES (NULL, "Thas'dorah, Legacy of the Windrunners", "thasDorah_1.jpg", 8), 
+                                (NULL, "A Sister's Bond", "aSisterSBond_1.jpg", 8), 
+                                (NULL, "Phoenix's Rebirth", "phoenixSRebirth_1.jpg", 8), 
+                                (NULL, "Ranger-General's Guard", "rangerGeneralSGuard_1.jpg", 8);
 
-INSERT INTO `appearance` VALUES (NULL, "Talonclaw", "talonclaw_red.jpg", 9), 
-                (NULL, "Eagle's Rebirth", "eagleSRebirth_orange.jpg", 9), 
-                (NULL, "Spear of the Alpha", "spearOfTheAlpha_red.jpg", 9), 
-                (NULL, "Serpentstrike", "serpentstrike_blue.jpg", 9);
+INSERT INTO `appearance` VALUES (NULL, "Talonclaw", "talonclaw_1.jpg", 9), 
+                                (NULL, "Eagle's Rebirth", "eagleSRebirth_1.jpg", 9), 
+                                (NULL, "Spear of the Alpha", "spearOfTheAlpha_1.jpg", 9), 
+                                (NULL, "Serpentstrike", "serpentstrike_1.jpg", 9);
 
-INSERT INTO `appearance` VALUES (NULL, "The Kingslayers", "theKingslayers_red.jpg", 10), 
-                (NULL, "Cursed Hand", "cursedHand_red.jpg", 10), 
-                (NULL, "Heartstopper", "heartstopper_green.jpg", 10), 
-                (NULL, "Magekiller's Edge", "magekillerSEdge_orange.jpg", 10);
+INSERT INTO `appearance` VALUES (NULL, "The Kingslayers", "theKingslayers_1.jpg", 10), 
+                                (NULL, "Cursed Hand", "cursedHand_1.jpg", 10), 
+                                (NULL, "Heartstopper", "heartstopper_1.jpg", 10), 
+                                (NULL, "Magekiller's Edge", "magekillerSEdge_1.jpg", 10);
 
-INSERT INTO `appearance` VALUES (NULL, "The Dreadblades", "theDreadblades_white.jpg", 11), 
-                (NULL, "Promise of the Seascourge", "promiseOfTheSeascourge_black.jpg", 11), 
-                (NULL, "Flame's Kiss", "flameSKiss_orange.jpg", 11), 
-                (NULL, "Scoundrel's Last Word", "scoundrelSLastWord_green.jpg", 11);
+INSERT INTO `appearance` VALUES (NULL, "The Dreadblades", "theDreadblades_1.jpg", 11), 
+                                (NULL, "Promise of the Seascourge", "promiseOfTheSeascourge_1.jpg", 11), 
+                                (NULL, "Flame's Kiss", "flameSKiss_1.jpg", 11), 
+                                (NULL, "Scoundrel's Last Word", "scoundrelSLastWord_1.jpg", 11);
+
+INSERT INTO `appearance` VALUES (NULL, "Fangs of the Devourer", "fangsOfTheDevourer_1.jpg", 12), 
+                                (NULL, "Shadowblade", "shadowblade_1.jpg", 12), 
+                                (NULL, "Demon's Embrace", "demonSEmbrace_1.jpg", 12), 
+                                (NULL, "Bloodfeaster", "bloodfeaster_1.jpg", 12);
+
+INSERT INTO `appearance` VALUES (NULL, "Light's Wrath", "lightSWrath_1.jpg", 13), 
+                                (NULL, "Crest of the Redeemed", "crestOfTheRedeemed_1.jpg", 13), 
+                                (NULL, "Chalice of Light", "chaliceOfLight_1.jpg", 13), 
+                                (NULL, "Eternal Vigil", "eternalVigil_1.jpg", 13);
+
+INSERT INTO `appearance` VALUES (NULL, "T'uure, Beacon of the Naaru", "tUure_1.jpg", 14), 
+                                (NULL, "Banner of Purity", "bannerOfPurity_1.jpg", 14), 
+                                (NULL, "Keeper of Light", "keeperOfLight_1.jpg", 14), 
+                                (NULL, "Embrace of the Void", "embraceOfTheVoid_1.jpg", 14);
+
+INSERT INTO `appearance` VALUES (NULL, "Xal'atath, Blade of the Black Empire", "xalAtath_1.jpg", 15), 
+                                (NULL, "Embrace of the Old Gods", "embraceOfTheOldGods_1.jpg", 15), 
+                                (NULL, "The Fallen Blade", "theFallenBlade_1.jpg", 15), 
+                                (NULL, "Vision of Madness", "visionOfMadness_1.jpg", 15);
+
+INSERT INTO `appearance` VALUES (NULL, "Maw of the Damned", "mawOfTheDamned_1.jpg", 16), 
+                                (NULL, "Bloodmaw", "bloodmaw_1.jpg", 16), 
+                                (NULL, "Soulreaper", "soulreaper_1.jpg", 16), 
+                                (NULL, "Executioner", "executioner_1.jpg", 16);
+
+INSERT INTO `appearance` VALUES (NULL, "Blades of the Fallen Prince", "bladesOfTheFallenPrince_1.jpg", 17), 
+                                (NULL, "Frostmourne's Legacy", "frostmourneSLegacy_1.jpg", 17), 
+                                (NULL, "Sindragosa's Fury", "sindragosaSFury_1.jpg", 17), 
+                                (NULL, "Gravekeeper", "gravekeeper_1.jpg", 17);
+
+INSERT INTO `appearance` VALUES (NULL, "Apocalypse", "apocalypse_1.jpg", 18), 
+                                (NULL, "Unholy War", "unholyWar_1.jpg", 18), 
+                                (NULL, "Herald of Pestilence", "heraldOfPestilence_1.jpg", 18), 
+                                (NULL, "Faminebearer", "faminebearer_1.jpg", 18);
+
+INSERT INTO `appearance` VALUES (NULL, "The Fist of Ra-den", "theFistOfRaDen_1.jpg", 19), 
+                                (NULL, "Stormkeeper", "stormkeeper_1.jpg", 19), 
+                                (NULL, "Earthspeaker", "earthspeaker_1.jpg", 19), 
+                                (NULL, "Fist of the Fallen Shaman", "fistOfTheFallenShaman.jpg", 19);
+
+INSERT INTO `appearance` VALUES (NULL, "Doomhammer", "doomhammer_1.jpg", 20), 
+                                (NULL, "Stormbringer", "stormbringer_1.jpg", 20), 
+                                (NULL, "Legion's Doom", "legionSDoom_1.jpg", 20), 
+                                (NULL, "Blackhand's Fate", "blackhandSFate_1.jpg", 20);
+
+INSERT INTO `appearance` VALUES (NULL, "Sharas'dal, Scepter of Tides", "sharasDal_1.jpg", 21), 
+                                (NULL, "Scepter of the Deep", "scepterOfTheDeep_1.jpg", 21), 
+                                (NULL, "Titanborn", "titanborn_1.jpg", 21), 
+                                (NULL, "Totembearer", "totembearer_1.jpg", 21);
+
+INSERT INTO `appearance` VALUES (NULL, "Aluneth", "aluneth_1.jpg", 22), 
+                                (NULL, "Guardian Spire", "guardianSpire_1.jpg", 22), 
+                                (NULL, "Magna Unleashed", "magnaUnleashed_1.jpg", 22), 
+                                (NULL, "Aegwynn's Fall", "aegwynnSFall_1.jpg", 22);
+
+INSERT INTO `appearance` VALUES (NULL, "Felo'melorn", "feloMelorn_1.jpg", 23), 
+                                (NULL, "Pride of the Sunstriders", "prideOfTheSunstriders_1.jpg", 23), 
+                                (NULL, "Phoenix's Rebirth", "phoenixSRebirth_1.jpg", 23), 
+                                (NULL, "Lavaborn Edge", "lavabornEdge_1.jpg", 23);
