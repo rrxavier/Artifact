54d4534d1b566831bc6b4766ca718541337e3a82
svenwikberg <sven.wkbrg@eduge.ch>
2016-10-06 14:13:33 +0200

    RemplissageAppearance

--- Doc/scriptRemplissage/remplissageAppearance.sql
+++ Doc/scriptRemplissage/remplissageAppearance.sql
@@ -112,3 +112,68 @@ INSERT INTO `appearance` VALUES (NULL, "Felo'melorn", "feloMelorn_1.jpg", 23),
                                 (NULL, "Pride of the Sunstriders", "prideOfTheSunstriders_1.jpg", 23), 
                                 (NULL, "Phoenix's Rebirth", "phoenixSRebirth_1.jpg", 23), 
                                 (NULL, "Lavaborn Edge", "lavabornEdge_1.jpg", 23);
+
+INSERT INTO `appearance` VALUES (NULL, "Ebonchill", "ebonchill_1.jpg", 24), 
+								(NULL, "Guardian's Focus", "guardianSFocus_1.jpg", 24), 
+								(NULL, "Flow of the First", "flowOfTheFirst_1.jpg", 24), 
+								(NULL, "Archmagi's Will", "archmagiSWill_1.jpg", 24);
+
+INSERT INTO `appearance` VALUES (NULL, "Ulthalesh, The Deadwind Harvester", "deadwindHarvester_1.jpg", 25), 
+								(NULL, "Hand of the Afflicted", "handOfTheAfflicted_1.jpg", 25), 
+								(NULL, "Soul Siphon", "soulSiphon_1.jpg", 25), 
+								(NULL, "Death's Hand", "deathSHand_1.jpg", 25);
+
+INSERT INTO `appearance` VALUES (NULL, "Skull of the Man'ari", "skullOfTheManAri_1.jpg", 26), 
+								(NULL, "Gaze of the First Summoner", "gazeOftheFirstSummoner_1.jpg", 26), 
+								(NULL, "Pride of the Pit Lord", "prideOfThePitLord_1.jpg", 26), 
+								(NULL, "Burning Remnant", "burningRemnant_1.jpg", 26);
+
+INSERT INTO `appearance` VALUES (NULL, "Scepter of Sargeras", "scepterOfSargeras_1.jpg", 27), 
+								(NULL, "Hubris of the Dark Titan", "hubrisOfTheDarkTitan_1.jpg", 27), 
+								(NULL, "Echo of Gul'dan", "echoOfGulDan_1.jpg", 27), 
+								(NULL, "Shadow of the Destroyer", "shadowOfTheDestroyer_1.jpg", 27);
+
+INSERT INTO `appearance` VALUES (NULL, "Fu Zan, the Wanderer's Companion", "fuZan_1.jpg", 28), 
+								(NULL, "The Monkey King's Burden", "theMonkeyKingSBurden_1.jpg", 28), 
+								(NULL, "Heart of the Ox", "heartOfTheOx_1.jpg", 28), 
+								(NULL, "Dragonfire's Grasp", "dragonfireSGrasp_1.jpg", 28);
+
+INSERT INTO `appearance` VALUES (NULL, "Sheilun, Staff of the Mists", "sheilun_1.jpg", 29), 
+								(NULL, "Toll of the Deep Mist", "TollOfTheDeepMist_1.jpg", 29), 
+								(NULL, "Chi-Ji's Spirit", "chiJiSSpirit_1.jpg", 29), 
+								(NULL, "Sha's Torment", "shaSTorment_1.jpg", 29);
+
+INSERT INTO `appearance` VALUES (NULL, "Fists of the Heavens", "fistsOfTheHeavens_1.jpg", 30), 
+								(NULL, "Al'Akir's Touch", "alAkirSTouch_1.jpg", 30), 
+								(NULL, "Spirit's Reach", "spiritSReach_1.jpg", 30), 
+								(NULL, "Shado-Pan Legacy", "shadoPanLegacy_1.jpg", 30);
+								
+INSERT INTO `appearance` VALUES (NULL, "Scythe of Elune", "scytheOfElune_1.jpg", 31), 
+								(NULL, "Envoy of Goldrinn", "envoyOfGoldrinn_1.jpg", 31), 
+								(NULL, "Lunarcall", "lunarcall_1.jpg", 31), 
+								(NULL, "Nightmare's Affliction", "nightmareSAffliction_1.jpg", 31);
+
+INSERT INTO `appearance` VALUES (NULL, "Fangs of Ashamane", "fangsOfAshamane_1.jpg", 32), 
+								(NULL, "Nature's Fury", "natureSFury_1.jpg", 32), 
+								(NULL, "Primal Stalker", "primalStalker_1.jpg", 32), 
+								(NULL, "Incarnation of Nightmare", "incarnationOfNightmare_1.jpg", 32);
+
+INSERT INTO `appearance` VALUES (NULL, "Claws of Ursoc", "clawsOfUrsoc_1.jpg", 33), 
+								(NULL, "Stonepaw", "stonepaw_1.jpg", 33), 
+								(NULL, "Avatar of Ursol", "avatarOfUrsol_1.jpg", 33), 
+								(NULL, "Fallen to Nightmare", "fallenToNightmare_1.jpg", 33);
+
+INSERT INTO `appearance` VALUES (NULL, "G'Hanir, the Mother Tree", "GHanir_1.jpg", 34), 
+								(NULL, "Eldertree", "eldertree_1.jpg", 34), 
+								(NULL, "Crystalline Awakening", "crystallineAwakening_1.jpg", 34), 
+								(NULL, "Deadwood Keeper", "deadwoodKeeper_1.jpg", 34);
+
+INSERT INTO `appearance` VALUES (NULL, "Twinblades of the Deceiver", "twinbladesOfTheDeceiver_1.jpg", 35), 
+								(NULL, "Hand of the Illidari", "handOfTheIllidari_1.jpg", 35), 
+								(NULL, "Darkenblade", "darkenblade_1.jpg", 35), 
+								(NULL, "Demon's Touch", "demonSTouch_1.jpg", 35);
+								
+INSERT INTO `appearance` VALUES (NULL, "Aldrachi Warblades", "aldrachiWarblades_1.jpg", 36), 
+								(NULL, "Illidari Crest", "illidariCrest_1.jpg", 36), 
+								(NULL, "Dreadlord's Bite", "dreadlordSBite_1.jpg", 36), 
+								(NULL, "Boneterror", "boneterror_1.jpg", 36);
