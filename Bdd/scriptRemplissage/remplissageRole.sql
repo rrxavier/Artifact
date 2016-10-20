e64c8d998b4097d5f0b675b1bb0109a070800c97
svenwikberg <sven.wkbrg@eduge.ch>
2016-10-20 15:23:57 +0200

    Debug git 1

--- Bdd/scriptRemplissage/remplissageRole.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `role`(`codeRole`, `name`) VALUES (1,"Damage Dealer");
-INSERT INTO `role`(`codeRole`, `name`) VALUES (2,"Tank");
-INSERT INTO `role`(`codeRole`, `name`) VALUES (3,"Healer");
